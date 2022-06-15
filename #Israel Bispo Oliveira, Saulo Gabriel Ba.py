319d255d2a1a798b698a88ce13e6a9eb6698469e
kenkpk <107439494+kenkpk@users.noreply.github.com>
2022-06-15 18:31:02 -0300

    Add files via upload

---
+++ #Israel Bispo Oliveira, Saulo Gabriel Ba.py
@@ -0,0 +1,7 @@
+#Israel Bispo Oliveira, Saulo Gabriel Baptista de Araujo#
+
+#importamos o módulo "os" e usamos o comando sytem para acessar o paint#
+import os
+os.system('mspaint.exe')
+
+x^2+y^2=
